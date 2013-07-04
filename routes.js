c5eb89f4c1b453f18b1d18446acad2b022cfb92a
Jasdev Singh <jsingh@twitter.com>
2013-07-03 23:43:37 -0700

    get users, need to paginate still

--- routes.js
+++ routes.js
@@ -1,11 +1,6 @@
-
-/*
- * GET home page.
- */
-
 var api = require('instagram-node').instagram();
 
-var redirect_uri = 'http://fathomless-wave-9392.herokuapp.com/handleauth';
+var redirect_uri = 'http://localhost:3000/handleauth';
 
 exports.authorize_user = function(req, res) {
   api.use({
@@ -22,8 +17,10 @@ exports.handleauth = function(req, res) {
       console.log(err);
       res.send("Didn't work" + err);
     } else {
-      console.log('Yay! Access token is ' + result.access_token);
-      res.send('You made it!!');
+      api.user_follows(result.user.id, function(err, users, pagination, limit) {
+        console.log(pagination.next_url);
+        res.send(users);
+      });
     }
   });
 };
